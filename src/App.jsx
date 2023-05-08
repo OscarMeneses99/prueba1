2a289df20a91fed5d64743b581be2f0a373e9425
Oscar Meneses <omenesess1800@alumno.ipn.mx>
2023-05-08 14:46:52 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1>Hola</h1>  
+    <input type="text" name="name" id="name" />
+<img className='logo' src={reactLogo} alt="react logo" width="300" />
+    </>
+  )
+}
+
+export default App
